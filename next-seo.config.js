82da3a1ae449dd3edadefc3aff9b216a5aa8f786
gowtham-prog <gowthamkumarvulluri@gmail.com>
2025-02-23 22:13:36 +0530

    init seo

---
+++ next-seo.config.js
@@ -0,0 +1,27 @@
+export default {
+    title: "Gowtham Kumar Vulluri | Full Stack Developer (Django-React)",
+    description:
+        "Full Stack Developer with 2+ years of experience in Django, React, AWS. Available for freelance and full-time roles. Explore my portfolio & contact me!",
+    openGraph: {
+        type: "website",
+        locale: "en_US",
+        url: "https://gowthamkv-full-stack-developer.vercel.app/",
+        title: "Gowtham Kumar Vulluri | Full Stack Developer (Django-React)",
+        description:
+            "Full Stack Developer specializing in Django and React. Hire me for freelance or full-time roles!",
+        // images: [
+        //     {
+        //         url: "https://gowthamkv-full-stack-developer.vercel.app/og.png",
+        //         width: 1200,
+        //         height: 630,
+        //         alt: "Gowtham Kumar Vulluri Portfolio",
+        //     },
+        // ],
+        site_name: "Gowtham Kumar Vulluri Portfolio",
+    },
+    // twitter: {
+    //     handle: "@your_twitter",
+    //     site: "@your_twitter",
+    //     cardType: "summary_large_image",
+    // },
+};
